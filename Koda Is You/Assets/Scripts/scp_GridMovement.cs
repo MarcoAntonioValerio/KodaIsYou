2971f492864e73462b30894816a625c9c8f13cb4
mav90 <marco.valerio@hotmail.it>
2020-04-27 12:20:12 +0100

    Reverted to translation over rigidbody, now player stops when it collides with Wall.

--- Koda Is You/Assets/Scripts/scp_GridMovement.cs
+++ Koda Is You/Assets/Scripts/scp_GridMovement.cs
@@ -12,44 +12,41 @@ public class scp_GridMovement : MonoBehaviour
 
 
 
-    [Header("moving Values")]
+    [Header("Moving Values")]
     public float moveSpeed = 5f;
     public Transform movePoint;
+    [Header("Collision Stuff")]
+    public LayerMask whatStopsMovement;
+    public float raycastCircleSize = 0.2f;
 
 
     private GameObject whoIsMovingNow;
     private scp_KodaAnimationTransition kodaAnimationScript;
     private bool isMoving = false;
     private bool isFacingRight = true;
-    private Vector2 movement;
+    
 
 
     //-------------------------------------------
     void Start()
     {
-        SetupStuff();
+        VariablesInitialisation();
     }     
 
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-    }
-    
-    private void FixedUpdate()
-    {
         var horizontal = Input.GetAxisRaw("Horizontal");
-        if (isMoving == false)
-        {
-            Moving2(movement);
-        }
+        Moving();        
         FlipSprite(horizontal);
     }
+    
+    
     //----------------------------------------------------------
 
-    private void SetupStuff()
+    private void VariablesInitialisation()
     {
+        //
         isFacingRight = true;
         movePoint.parent = null;
         whoIsMovingNow = gameObject;
@@ -57,64 +54,69 @@ public class scp_GridMovement : MonoBehaviour
     }
     private void Moving()
     {
-
+        //Checking that whatIsMovingNow is not empty
         if (whoIsMovingNow != null)
         {
+            //Translation code
             whoIsMovingNow.transform.position = Vector3.MoveTowards(whoIsMovingNow.transform.position, movePoint.position,
                                                             moveSpeed * Time.deltaTime);
 
-            //Vector2 pos = movePoint.position;
-
-            //whoIsMovingNow.transform.position = this.GetComponent<Rigidbody2D>().MovePosition(pos);
-
-            if (Vector3.Distance(whoIsMovingNow.transform.position, movePoint.position) == 0f)
+            
+            //If the sprite has completely reached the destination
+            if (Vector3.Distance(whoIsMovingNow.transform.position, movePoint.position) <= 0.05f)
             {
-
-                if (Input.GetAxisRaw("Horizontal") == 1f || Input.GetAxisRaw("Horizontal") == -1f)
+                //Horizontal Movement
+                if (Mathf.Abs( Input.GetAxisRaw("Horizontal")) == 1f)
                 {
-                    isMoving = true;
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-
-                    if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Horizontal") == 1f)
+                        //Raycast to check if the next position is occupied by a collider.
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), raycastCircleSize, whatStopsMovement))
                     {
-                        kodaAnimationScript.KodaAnimationRight();
-                        //FlipSprite(Input.GetAxisRaw("Horizontal"));
+                        //Updating the position of the movePoint
+                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                        
+                        //Animations
+                        if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Horizontal") == 1f)
+                        {
+                            kodaAnimationScript.KodaAnimationRight();
 
-                    }
-                    if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Horizontal") == -1f)
-                    {
-                        FlipSprite(Input.GetAxisRaw("Horizontal"));
-                        kodaAnimationScript.KodaAnimationLeft();
+                        }
+                        if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Horizontal") == -1f)
+                        {                            
+                            kodaAnimationScript.KodaAnimationLeft();
 
+                        }
                     }
-                    isMoving = false;
                 }
+                //Vertical movement
                 else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
                 {
-                    isMoving = true;
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                    if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Vertical") == 1)
+                        //Raycast to check if the next position is occupied by a collider.
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), raycastCircleSize, whatStopsMovement))
                     {
-                        kodaAnimationScript.KodaAnimationUp();
-                    }
-                    if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Vertical") == -1)
-                    {
-                        kodaAnimationScript.KodaAnimatinionDown();
-                    }
-                    isMoving = false;
-                }
-                else
-                {
-                    if (whoIsMovingNow.tag == "Player")
-                    {
-                        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
+                        //Updating the position of the movePoint
+                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                        
+                        //Animations
+                        if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Vertical") == 1)
                         {
-                            kodaAnimationScript.KodaAnimationIdle();
+                            kodaAnimationScript.KodaAnimationUp();
                         }
-
-
+                        if (whoIsMovingNow.tag == "Player" && Input.GetAxisRaw("Vertical") == -1)
+                        {
+                            kodaAnimationScript.KodaAnimatinionDown();
+                        }
                     }
+                        
                 }
+
+                //Idle
+                else if ((Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0) && whoIsMovingNow.tag == "Player")
+                {
+                    kodaAnimationScript.KodaAnimationIdle();
+                }
+
+
+
             }
         }
         
@@ -122,10 +124,7 @@ public class scp_GridMovement : MonoBehaviour
         
     }
 
-    private void Moving2(Vector2 direction)
-    {
-        GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
-    }
+    
 
     private void FlipSprite(float horizontal)
     {
